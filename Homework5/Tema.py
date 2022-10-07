adef6986b7fc4bb8e5b61fe6266b5bbf9d2dfb71
IsfanMatei <113927959+IsfanMatei@users.noreply.github.com>
2022-10-07 16:23:57 +0300

    Numele, nota si materiile

---
+++ Homework5/Tema.py
@@ -0,0 +1,12 @@
+a = input("Introdu numele tau: ")
+b = int(input("Nota la istorie: "))
+c = int(input("Nota la romana: "))
+d = int(input("Nota la matematica: "))
+e = int(input("Nota la biologie: "))
+total = b+c+d+e
+medie = total/4
+print("Te numesti ", a)
+print("Notele tale sunt: ", b,c,d,e)
+print("Notele tale adunate fac in total: ",total)
+print("Nota medie este: ",medie)
+
